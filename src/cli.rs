d31d4763c45ee099899f555eedb11271f35545b8
ArnabRollin <iamarnab.phukan@gmail.com>
2024-03-04 14:13:47 -0800

    Added CLI

---
+++ src/cli.rs
@@ -0,0 +1,23 @@
+use clap::{Parser, Subcommand};
+
+/// The Orion CLI
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub(crate) struct Args {
+    #[command(subcommand)]
+    pub(crate) command: Commands
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum Commands {
+    /// Run an Orion File.
+    Run {
+        #[arg(long, help = "The file to run.")]
+        file: String
+    },
+    /// Generate completions for a shell
+    Complete {
+        #[arg(short, long, help = "The shell to generate completions for.")]
+        shell: String
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+use std::io;
+use std::process::exit;
+use clap::{CommandFactory, Parser};
+use clap_complete::generate;
+use clap_complete::shells::{Bash, Zsh, Fish, PowerShell, Elvish};
+use orion_lib::run_contents;
+use crate::cli::{Args, Commands};
+
+mod cli;
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Run { file } => {
+            let contents = read_to_string(file)?;
+            run_contents(contents);
+        }
+        Commands::Complete { shell } => {
+            match shell.to_lowercase().as_str() {
+                "bash" => generate(Bash, &mut Args::command(), "orion", &mut io::stdout()),
+                "zsh" => generate(Zsh, &mut Args::command(), "orion", &mut io::stdout()),
+                "fish" => generate(Fish, &mut Args::command(), "orion", &mut io::stdout()),
+                "powershell" => generate(PowerShell, &mut Args::command(), "orion", &mut io::stdout()),
+                "elvish" => generate(Elvish, &mut Args::command(), "orion", &mut io::stdout()),
+                shell => {
+                    eprintln!("Error: Invalid shell `{}`!", shell);
+                    exit(1);
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
